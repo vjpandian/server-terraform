5fddc0b483f5241847f144d467fd9421b4aedeed
soulchips <akil@circleci.com>
2021-07-19 13:30:25 -0400

    updating start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# set TLS env vars
+echo "export TF_VAR_nomad_server_cert=$(cat /etc/nomad/ssl/cert.pem)" >> ~/.bashrc
+echo "export TF_VAR_nomad_server_key=$(cat /etc/nomad/ssl/key.pem)" >> ~/.bashrc
+echo "export TF_VAR_nomad_tls_ca=$(cat /etc/nomad/ssl/ca.pem)" >> ~/.bashrc
+source ~/.bashrc
 
 cd nomad-aws
 terraform init
